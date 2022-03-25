71d6843b693defa8adcbb6abc38a4ec7727e078a
Alicia Thomas <alicia@ondibs.com>
2022-03-24 18:37:35 -0700

    working - shows 3 cards

--- src/views/studio-admin/dashboard/index.js
+++ src/views/studio-admin/dashboard/index.js
@@ -1,5 +1,6 @@
 // material-ui
 import { Typography, Grid, Paper, Box } from '@mui/material';
+import EarningCard from '../../../components/EarningCard';
 
 // project imports
 import { useTheme } from '@emotion/react';
@@ -8,6 +9,28 @@ import { useTheme } from '@emotion/react';
 
 const Dashboard = () => {
     const theme = useTheme();
+    const dataValues = [
+        {
+            id: 1,
+            title: 'Today',
+            revenue: '$26',
+            comparedwith: 'yesterday'
+        },
+        {
+            id: 2,
+            title: 'This week',
+            revenue: '$250',
+            comparedwith: 'last week'
+        },
+        {
+            id: 3,
+            title: 'This month',
+            revenue: '12829',
+            comparedwith: 'lastmonth'
+        }
+    ];
+    console.log('data-values');
+    console.log(JSON.stringify(dataValues));
     return (
         <Paper
             sx={{
@@ -16,12 +39,13 @@ const Dashboard = () => {
                 elevation: 0
             }}
         >
-            <Grid container xs={12}>
+            <Grid container>
                 <Box
                     xs={12}
                     sx={{
                         marginTop: '20px',
                         marginLeft: '25px',
+                        marginRight: '20px',
                         display: 'flex',
                         flexDirection: 'row',
                         width: '100%'
@@ -31,6 +55,16 @@ const Dashboard = () => {
                         <Typography gutterBottom variant="h3">
                             SALES REVENUE
                         </Typography>
+                        <Typography gutterBottom variant="h6">
+                            Revenue generated from purchases (retail, package, classes).
+                        </Typography>
+                        <Grid container>
+                            {dataValues.map((value, i) => (
+                                <Grid key={i} item>
+                                    <EarningCard data={value} />
+                                </Grid>
+                            ))}
+                        </Grid>
                     </Grid>
                     <Grid item xs={3}>
                         <Typography gutterBottom variant="h3">
