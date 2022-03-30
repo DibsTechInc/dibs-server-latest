4be6d14032a29e45f616d81afbef3beb8b4ea45c
Alicia Thomas <alicia@ondibs.com>
2022-03-30 15:12:18 -0400

    I am connecting to the database

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { Sequelize } = require('@sequelize/core');
 require('dotenv').config();
+const settings = require('./models/config/config');
 
-// const db = require('./models');
+if (process.env.NODE_ENV !== 'production') {
+    // eslint-disable-next-line
+    require('dotenv').config();
+}
+const env = process.env.NODE_ENV || 'development';
+const config = settings[env];
+// const db = {};
 
-// db.sequelize.sync();
+const sequelize =
+    env === 'production'
+        ? new Sequelize(process.env.DATABASE_URL, config)
+        : new Sequelize(config.database, config.username, config.password, config);
 
 const app = express();
 
@@ -13,6 +24,15 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
+    async function getData() {
+        try {
+            await sequelize.authenticate();
+            console.log('Connection has been established successfully.');
+        } catch (error) {
+            console.error('Unable to connect to the database:', error);
+        }
+    }
+    getData();
     res.json({
         message: 'Welcome to Dibs Base API Back End Start.'
     });
