f10fb9591cbf4be8aaf21ab6e27c371a22296ed8
Alicia Thomas <alicia@ondibs.com>
2022-04-03 15:33:37 -0400

    connecting successfully to the database

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const models = require('@dibs-tech/models');
+
+const {
+    Sequelize: { Op }
+} = models;
 
 const app = express();
 
@@ -12,6 +17,24 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
+    async function getEvents() {
+        try {
+            const events = await models.event.findAll({
+                where: {
+                    dibs_studio_id: 218,
+                    canceled: 0,
+                    deleted: 0,
+                    start_date: {
+                        [Op.gte]: '2022-03-22 00:00:00'
+                    }
+                }
+            });
+            console.log(`events from db are: ${JSON.stringify(events)}`);
+        } catch (err) {
+            console.log(`error in getEvents: ${err}`);
+        }
+    }
+    getEvents();
     res.json({
         message: 'Welcome to Dibs Base API Back End Start.'
     });
