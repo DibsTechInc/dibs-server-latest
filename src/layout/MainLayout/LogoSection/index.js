9d9048359769ead65d1ba36b55f33d4720597f20
Alicia Thomas <alicia@ondibs.com>
2022-04-06 10:26:26 -0400

    changed the logo to dibs logo

--- src/layout/MainLayout/LogoSection/index.js
+++ src/layout/MainLayout/LogoSection/index.js
@@ -5,13 +5,15 @@ import { Link } from '@mui/material';
 
 // project imports
 import { DASHBOARD_PATH } from 'config';
-import Logo from 'ui-component/Logo';
+// import Logo from 'ui-component/Logo';
+import Logo from 'ui-component/dibslogo.png';
 
 // ==============================|| MAIN LOGO ||============================== //
 
 const LogoSection = () => (
     <Link component={RouterLink} to={DASHBOARD_PATH}>
-        <Logo />
+        {/* <Logo /> */}
+        <img src={Logo} alt="Logo" height="25" />
     </Link>
 );
 
