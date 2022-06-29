291b2d60455d8de9318999cccee706367296a381
Alicia Thomas <alicia@ondibs.com>
2022-06-29 17:55:56 -0400

    cleanup datepicker file

--- src/shared/components/DatePicker/datePickerNormal.js
+++ src/shared/components/DatePicker/datePickerNormal.js
@@ -16,13 +16,8 @@ export default function BasicDatePicker(props) {
     date.setDate(date.getDate());
     const { setDate, type } = props;
     const setDateForDatabase = (date) => {
-        console.log(`setDatefordatabase is being called: ${JSON.stringify(date)}`);
         if (type === 'start') {
-            console.log(`type is start`);
-            console.log(`timezone is: ${timeZone}`);
             const datetouse = moment(date).tz(timeZone);
-            console.log(`datetouse is: ${JSON.stringify(datetouse)}`);
-            console.log(`am setting date to: ${moment(datetouse).startOf('day').utc().format('YYYY-MM-DD HH:mm:ss')}`);
             setDate(moment(datetouse).startOf('day').utc().format('YYYY-MM-DD HH:mm:ss'));
         } else {
             const datetouse = moment(date).tz(timeZone);
@@ -54,7 +49,5 @@ export default function BasicDatePicker(props) {
 }
 BasicDatePicker.propTypes = {
     setDate: propTypes.func,
-    setHasError: propTypes.func,
-    setErrorMessage: propTypes.func,
     type: propTypes.string
 };
